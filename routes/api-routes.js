443a7d14dda7115f69572755594a5ccb5d7a5c35
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2021-02-10 15:53:23 -0800

    set up initial api routes and mongoose Workout Schema

---
+++ routes/api-routes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+
+router.get("/api/workouts", () => {
+
+});
+
+router.put("/api/workouts/:id", () => {
+
+});
+
+router.post("/api/workouts", () => {
+
+});
+
+router.get("/api/workouts/range", () => {
+
+});
+
+module.exports = router;
